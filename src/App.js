434da286605fbf1478f698ebf4111dfdb4cf9778
Jaime Jacobo <jaimejacobo@Jaimes-Mac-mini.local>
2022-10-23 23:12:13 +0900

    extracted and created PokemonSelector components

--- src/App.js
+++ src/App.js
@@ -8,18 +8,11 @@ import './App.scss'
 import { useGetFormattedPokemon } from './hooks/useGetFormattedPokemon'
 
 // Components
-import PokemonImage from './components/PokemonImage.jsx'
 import GenerationSelector from './components/GenerationSelector.jsx'
+import PokemonSelector from './components/PokemonSelector.jsx'
 
 // Material UI components
-import Box from '@mui/material/Box'
-import TextField from '@mui/material/TextField'
-import Autocomplete from '@mui/material/Autocomplete'
-import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
-
-// Material UI icons
-import SendIcon from '@mui/icons-material/Send'
 
 const App = () => {
   const pokemons = useGetFormattedPokemon()
@@ -42,38 +35,10 @@ const App = () => {
           selectedGenerations={selectedGenerations}
           setSelectedGenerations={setSelectedGenerations}
         />
-        <div className="App__pokemon-selector">
-          <Autocomplete
-            sx={{ width: 300 }}
-            options={pokemons}
-            autoHighlight
-            getOptionLabel={(pokemon) => pokemon.name}
-            onChange={(event) => setSelectedPokemon(event.target.innerText)}
-            renderOption={(props, pokemon) => (
-              <Box
-                component="li"
-                sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
-                {...props}
-              >
-                <PokemonImage pokemon={pokemon} />
-                {pokemon.name}
-              </Box>
-            )}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Choose a pokemon"
-                inputProps={{
-                  ...params.inputProps,
-                  autoComplete: 'new-password', // disable autocomplete and autofill
-                }}
-              />
-            )}
-          />
-          <Button variant="contained" endIcon={<SendIcon />}>
-            Send
-          </Button>
-        </div>
+        <PokemonSelector
+          pokemons={pokemons}
+          setSelectedPokemon={setSelectedPokemon}
+        />
         <p>{pokemons.length} pokemons loaded</p>
       </Container>
     </div>
---
+++ src/components/PokemonSelector.jsx
@@ -0,0 +1,51 @@
+// Components
+import PokemonImage from './PokemonImage.jsx'
+
+// Material UI components
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import Autocomplete from '@mui/material/Autocomplete'
+import Button from '@mui/material/Button'
+
+// Material UI icons
+import SendIcon from '@mui/icons-material/Send'
+
+const PokemonSelector = (props) => {
+  const { pokemons, setSelectedPokemon } = props
+  return (
+    <div className="App__pokemon-selector">
+      <Autocomplete
+        sx={{ width: 300 }}
+        options={pokemons}
+        autoHighlight
+        getOptionLabel={(pokemon) => pokemon.name}
+        onChange={(event) => setSelectedPokemon(event.target.innerText)}
+        renderOption={(props, pokemon) => (
+          <Box
+            component="li"
+            sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
+            {...props}
+          >
+            <PokemonImage pokemon={pokemon} />
+            {pokemon.name}
+          </Box>
+        )}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Choose a pokemon"
+            inputProps={{
+              ...params.inputProps,
+              autoComplete: 'new-password', // disable autocomplete and autofill
+            }}
+          />
+        )}
+      />
+      <Button variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button>
+    </div>
+  )
+}
+
+export default PokemonSelector
