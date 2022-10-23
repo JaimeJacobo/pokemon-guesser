35e687c173f367e9722f9433f29aa16af30025c9
Jaime Jacobo <jaimejacobo@Jaimes-Mac-mini.local>
2022-10-23 23:07:01 +0900

    extracted code to GenerationSelector component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
+// React
 import * as React from 'react'
+
+// CSS
 import './App.scss'
 
+// Hooks
 import { useGetFormattedPokemon } from './hooks/useGetFormattedPokemon'
 
+// Components
 import PokemonImage from './components/PokemonImage.jsx'
+import GenerationSelector from './components/GenerationSelector.jsx'
 
+// Material UI components
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 import Autocomplete from '@mui/material/Autocomplete'
-import FormGroup from '@mui/material/FormGroup'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Checkbox from '@mui/material/Checkbox'
 import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
 
+// Material UI icons
 import SendIcon from '@mui/icons-material/Send'
 
 const App = () => {
   const pokemons = useGetFormattedPokemon()
 
   const [selectedPokemon, setSelectedPokemon] = React.useState('')
-  const [selectedGeneration, setSelectedGeneration] = React.useState([
+  const [selectedGenerations, setSelectedGenerations] = React.useState([
     true,
     false,
     false,
@@ -30,102 +35,13 @@ const App = () => {
     false,
   ])
 
-  const handleSelectAllGenerations = (event) => {
-    setSelectedGeneration(
-      selectedGeneration.map((element) => event.target.checked)
-    )
-  }
-
-  const handleChangeGeneration = (event, index) => {
-    const selectedGenerationCopy = [...selectedGeneration]
-    selectedGenerationCopy[index] = event.target.checked
-    setSelectedGeneration(selectedGenerationCopy)
-  }
-
   return (
     <div className="App">
       <Container>
-        <FormGroup>
-          <div>
-            <p>GENERATIONS</p>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  color="secondary"
-                  checked={selectedGeneration.every((element) => element)}
-                  onChange={(event) => handleSelectAllGenerations(event)}
-                />
-              }
-              label="Select All"
-            />
-          </div>
-
-          <div className="App__generation-selector">
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={selectedGeneration[0]}
-                  onChange={(event) => handleChangeGeneration(event, 0)}
-                />
-              }
-              label="1"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={selectedGeneration[1]}
-                  onChange={(event) => handleChangeGeneration(event, 1)}
-                />
-              }
-              label="2"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={selectedGeneration[2]}
-                  onChange={(event) => handleChangeGeneration(event, 2)}
-                />
-              }
-              label="3"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={selectedGeneration[3]}
-                  onChange={(event) => handleChangeGeneration(event, 3)}
-                />
-              }
-              label="4"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={selectedGeneration[4]}
-                  onChange={(event) => handleChangeGeneration(event, 4)}
-                />
-              }
-              label="5"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={selectedGeneration[5]}
-                  onChange={(event) => handleChangeGeneration(event, 5)}
-                />
-              }
-              label="6"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={selectedGeneration[6]}
-                  onChange={(event) => handleChangeGeneration(event, 6)}
-                />
-              }
-              label="7"
-            />
-          </div>
-        </FormGroup>
+        <GenerationSelector
+          selectedGenerations={selectedGenerations}
+          setSelectedGenerations={setSelectedGenerations}
+        />
         <div className="App__pokemon-selector">
           <Autocomplete
             sx={{ width: 300 }}
---
+++ src/components/GenerationSelector.jsx
@@ -0,0 +1,63 @@
+// Material UI components
+import FormGroup from '@mui/material/FormGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
+
+const GenerationSelector = (props) => {
+  const { selectedGenerations, setSelectedGenerations } = props
+
+  const handleSelectAllGenerations = (event) => {
+    setSelectedGenerations(
+      selectedGenerations.map((element) => event.target.checked)
+    )
+  }
+
+  const handleChangeGenerations = (event, index) => {
+    const selectedGenerationsCopy = [...selectedGenerations]
+    selectedGenerationsCopy[index] = event.target.checked
+    setSelectedGenerations(selectedGenerationsCopy)
+  }
+
+  const renderGenerationSelector = () => {
+    return (
+      <div className="App__generation-selector">
+        {selectedGenerations.map((element, index) => {
+          return (
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={selectedGenerations[index]}
+                  onChange={(event) => handleChangeGenerations(event, index)}
+                />
+              }
+              label={index + 1}
+              labelPlacement="start"
+            />
+          )
+        })}
+      </div>
+    )
+  }
+
+  return (
+    <div className="GenerationSelector">
+      <FormGroup>
+        <p>GENERATIONS</p>
+        <FormControlLabel
+          control={
+            <Checkbox
+              color="secondary"
+              checked={selectedGenerations.every((element) => element)}
+              onChange={(event) => handleSelectAllGenerations(event)}
+            />
+          }
+          label="Select All"
+        />
+
+        {renderGenerationSelector()}
+      </FormGroup>
+    </div>
+  )
+}
+
+export default GenerationSelector
